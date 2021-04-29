31950a1ff7c682cc6480ca1545068c22d91bfb53
vjakupi <valon.jakupi@protonmail.com>
2021-04-29 11:44:54 -0400

    Added ability to have app list the current todos

--- todos.js
+++ todos.js
@@ -1,6 +1,15 @@
-let input = prompt("What would you like to do?")
+let input = prompt('What would you like to do?')
+const todos = ['Complete Todos project', 'Make roadmap for AWS CCP cert'];
 while (input !== 'quit' && input !== `q`) {
-    input = prompt("What would you like to do?")
+    if (input === 'list') {
+        console.log('**********')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('**********')
+
+    }
+    input = prompt('What would you like to do?')
 }
 console.log('Okay, you quit the app')
 
