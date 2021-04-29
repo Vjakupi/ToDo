bdbebe65889c75f72120522b4cd6cdfb0d293a8a
vjakupi <valon.jakupi@protonmail.com>
2021-04-29 12:00:26 -0400

    Added a function to delete by index as well as a console message telling user what has been deleted

--- todos.js
+++ todos.js
@@ -4,10 +4,18 @@ while (input !== 'quit' && input !== `q`) {
     if (input === 'list') {
         console.log('**********')
         for (let i = 0; i < todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`)
+            console.log(`${i}: ${todos[i]}`);
         }
         console.log('**********')
 
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list!`)
+    } else if (input === 'delete') {
+        const index = prompt('Ok, enter an index to delete?');
+        const deleted = todos.splice(index, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
     }
     input = prompt('What would you like to do?')
 }
